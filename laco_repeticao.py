be4e7ee5b082c7e2f02840f4b1e6df412fbcc440
thiago deones <thiago.deones@gmail.com>
2024-12-16 21:12:55 -0300

    acrescentei e retirei fork

--- laco_repeticao.py
+++ laco_repeticao.py
@@ -5,13 +5,13 @@ def texto(palavra):
     #percorrendo a string 
     for letra in palavra:
         if letra.upper() in VOGAIS:
-            print(letra, end="")
+            print(letra, end=", ")
 
     print()
 
 
-texto("Primeira palavra")
-texto("Segunda palavra")
+#texto("Primeira palavra: ")
+#texto("Segunda palavra: ")
 
 
 for numero in range(5,10):
@@ -21,3 +21,10 @@ print()
 
 for numero in range(10):
     print(numero, end = " |")
+
+print()
+
+#para fazer a tabuada
+for numero in range(0, 51, 5):
+    print(numero, end=" ")
+
